87f871e2653c5a76f690183ecdde295f8a42ee6c
Ewerton Victor <93969276+EwertonVictor@users.noreply.github.com>
2023-11-26 20:48:00 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -13,3 +13,21 @@ document.getElementById('sifu').addEventListener('click', function() {
 function redirecionarPara(url) {
     window.open(url, '_blank');
 }
+
+// teste
+
+var elementosBx = document.querySelectorAll('.bx');
+
+// Adicione um ouvinte de evento de clique a cada elemento
+elementosBx.forEach(function (elemento) {
+    elemento.addEventListener('click', function () {
+        // Obtenha o link associado ao elemento clicado
+        var link = elemento.querySelector('a');
+
+        // Verifique se há um link e redirecione para ele
+        
+        if (link) {
+            window.open(link.href, '_blank');
+        }
+    });
+});
